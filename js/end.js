ace962008dfd426b81616b5247355008349365cb
fchialvo <francochialvo03@gmail.com>
2022-11-13 00:07:46 -0300

    fix pages bug

--- js/end.js
+++ js/end.js
@@ -27,5 +27,5 @@ function saveHighScore(e){
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('./index.html');
 };
